9a1101d5a6210792fd825bf50cf60af8bf1f7956
thekey82 <74416112+thekey82@users.noreply.github.com>
2024-06-24 18:48:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 
 app = FastAPI()
 
-tasmota_ip = os.getenv('TASMOTA_IP', '172.16.90.72')
+tasmota_ip = os.getenv('SHELLY_IP', '192.168.178.205')
 
 # Define the data structure
 class Vitals(BaseModel):
@@ -37,35 +37,39 @@ class Vitals(BaseModel):
     current_alerts: list
 
 
-def get_tasmota_current(tasmota_ip):
-    url = f"http://{tasmota_ip}/cm?cmnd=status%208"
+def get_shelly_current(shelly_ip):
+    url = f"http://{shelly_ip}/rpc/Shelly.GetStatus"
+
     #{"StatusSNS":{"Time":"1970-01-01T17:54:20","ANALOG":{"Temperature":15.0},"ENERGY":{"TotalStartTime":"1970-01-01T00:00:00","Total":2.361,"Yesterday":0.000,"Today":2.361,"Power":2,"ApparentPower":13,"ReactivePower":12,"Factor":0.17,"Voltage":225,"Current":0.056},"TempUnit":"C"}}
+
+    #{"ble":{},"cloud":{"connected":true},"input:0":{"id":0,"state":false},"mqtt":{"connected":false},"switch:0":{"id":0, "source":"HTTP_in", "output":false, "apower":0.0, "voltage":248.8, "current":0.000, "aenergy":{"total":7840.485,"by_minute":[0.000,0.000,0.000],"minute_ts":1719228000},"temperature":{"tC":57.9, "tF":136.2}},"sys":{"mac":"B0B21C102CC4","restart_required":false,"time":"13:20","unixtime":1719228008,"uptime":36301,"ram_size":261564,"ram_free":122896,"fs_size":458752,"fs_free":139264,"cfg_rev":11,"kvs_rev":0,"schedule_rev":1,"webhook_rev":0,"available_updates":{},"reset_reason":3},"wifi":{"sta_ip":"192.168.178.205","status":"got ip","ssid":"Garage","rssi":-37},"ws":{"connected":false}}
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        return data["StatusSNS"]["ENERGY"]["Current"]
+        return data["voltage"]["output"]["current"]
     except requests.RequestException as e:
-        raise ValueError(f"Error fetching data from Tasmota device: {e}")
+        raise ValueError(f"Error fetching data from Shelly device: {e}")
 
-def get_tasmota_connected(tasmota_ip):
-    url = f"http://{tasmota_ip}/cm?cmnd=status%200"
+def get_shelly_connected(shelly_ip):
+    #url = f"http://{tasmota_ip}/cm?cmnd=status%200"
+    url = f"http://{shelly_ip}/rpc/Shelly.GetStatus"
     #{"Status":{'Module': 46, 'DeviceName': 'tm_gangOG', 'FriendlyName': [''], 'Topic': 'tasmota_12C771', 'ButtonTopic': '0', 'Power': 1, 'PowerOnState': 3, 'LedState': 1, 'LedMask': 'FFFF', 'SaveData': 1, 'SaveState': 1, 'SwitchTopic': '0', 'SwitchMode': [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'ButtonRetain': 0, 'SwitchRetain': 0, 'SensorRetain': 0, 'PowerRetain': 0, 'InfoRetain': 0, 'StateRetain': 0, 'StatusRetain': 0}
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        if data["Status"]["Power"] == 1:
+        if data["output"] == true:
             power = True
         else:
             power = False
         return power
     except requests.RequestException as e:
-        raise ValueError(f"Error fetching data from Tasmota device: {e}")
+        raise ValueError(f"Error fetching data from Shelly device: {e}")
 @app.get("/api/1/vitals")
 async def get_vitals():
     try:
-        current = get_tasmota_current(tasmota_ip)
+        current = get_shelly_current(shelly_ip)
     except ValueError as e:
         return {"error": str(e)}
     if current <= 4.5:
@@ -73,7 +77,7 @@ async def get_vitals():
     else:
         charging = True
     try:
-        connected = get_tasmota_connected(tasmota_ip)
+        connected = get_shelly_connected(shelly_ip)
     except ValueError as e:
         return {"error": str(e)}
     vitals = Vitals(
@@ -87,7 +91,7 @@ async def get_vitals():
         currentB_a=0.0,
         currentC_a=0.0,
         currentN_a=0.0,
-        voltageA_v=233,
+        voltageA_v=voltage,
         voltageB_v=0.0,
         voltageC_v=0.0,
         relay_coil_v=11.9,
