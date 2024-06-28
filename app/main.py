1b10055b8ed0b9686a2e5b3653c11f09fd235722
thekey82 <74416112+thekey82@users.noreply.github.com>
2024-06-28 14:14:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -104,7 +104,7 @@ async def get_vitals():
     else:
         charging = True
         session_energy_wh = total - wh
-        print("total")
+        print(total)
     try:
         connected = get_shelly_connected(shelly_ip)
     except ValueError as e:
